b7603629d76e961887488b6ae7b7e57ee5e08958
Santiago Fleitas <sansfleitas@gmail.com>
2021-10-05 18:07:37 -0300

    Adding componentization

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,26 @@
-export function SideBar() {
-  // Complete aqui
+import '../styles/sidebar.scss';
+
+import { Button } from '../components/Button';
+
+export function SideBar({ data }: any) {
+  const { selectedGenreId, genres } = data;
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map((genre: any) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => data.handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
